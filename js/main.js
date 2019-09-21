c850a42c4701b4ae0300cb2160a99bc9f3d26915
SvetlayaLana <amber.world9@gmail.com>
2019-09-21 20:07:54 +0300

    something done something not

--- js/main.js
+++ js/main.js
@@ -1,25 +1,107 @@
 console.log($(window).height());
-$('.main').css('height', $(window).height()-150);
+$(window).scrollTop(0);
+$('.main').css('height', $(window).height() - 150);
 $(window).resize(function () {
-    $('.main').css('height', $(window).height()-150);
+    $('.main').css('height', $(window).height() - 150);
 });
+
+console.log('scroll', $(window).scrollTop());
 
 $(window).scroll(function (e) {
-   console.log(e.screenY);
-   $.get('snippets/scene2-snippet.html', function (data) {
-       $('.main').html(data);
-   })
-});
+    let scrollTop = $(this).scrollTop();
+    console.log(scrollTop);
+    if (scrollTop <= 500 && $('.scene-1').length === 0) {
+        $('.main').fadeOut(function () {
+            $.get('snippets/scene1-snippet.html', function (data) {
+                $('.main').html(data).fadeIn();
+
+            })
+        });
+    } else if (scrollTop > 500 && scrollTop <= 1000) {
+        if ($('.scene-2').length === 0 || $('.scene-3').length !== 0) {
+            $('.main').fadeOut(function () {
+                // $('.ball').css('display', 'none');
+                $.get('snippets/scene2-snippet.html', function (data) {
+
+                    $('.main').html(data).fadeIn();
+
+                })
+            });
+        }
 
 
+    } else if (scrollTop > 1000 && scrollTop <= 1500 && $('.scene-3').length === 0) {
+        $('.scene-img-block img:nth-child(n+2)').fadeOut(700);
+        $('.scene-text').fadeOut(300, function () {
+            $.get('snippets/scene3-snippet.html', function (data) {
+                $('.scene-2').addClass('scene-3');
+                $('.scene-text').html(data).fadeIn(500,function () {
+                    $(".scene-img-block img:first-child").attr('src', 'img/screen3-photo.png').animate({
+                        maxWidth: '390px',
+                    }, 1000);
+                    $('.ball').fadeIn();
+                });
 
-// let scrollMagicController = new ScrollMagic();
+            });
+        });
+
+
+    }else if(scrollTop > 1500 && scrollTop <= 2000 && $('.scene-4').length === 0){
+        $('.ball').fadeOut();
+        $('.scene-text').fadeOut(300, function () {
+            $.get('snippets/scene4-snippet.html', function (data) {
+                $('.scene-3').addClass('scene-4');
+                $('.scene-text').html(data).fadeIn(500,function () {
+                    $(".scene-img-block img:first-child").attr('src', 'img/screen4-1.png').animate({
+                        maxWidth: '385px',
+                    }, 1000);
+
+                })
+            })
+        })
+
+
+    }
+
+
+    /*else if(scrollTop > 200 && $('.scene-3').length === 0){
+        $('.scene-img-block div:nth-child(n+2)').fadeOut();
+        $('.scene-2').fadeOut(300, function () {
+
+            $.get('snippets/scene3-snippet.html', function (data) {
+                $('.scene-text').html(data).fadeIn(500);
+            });
+            $(this).addClass('scene-3');
+
+            $(".scene-img-block div:first-child").css('background-image', 'url(img/screen3-photo.png)').fadeIn(function () {
+
+                $(this).animate({
+                    width: '390px',
+                    height: '390px',
+                }, 500)
+
+
+            });
+        }).fadeIn(500, function () {
+            $('.ball').fadeIn();
+        });
+    }*//*else{
+       $.get('snippets/scene1-snippet.html', function (data) {
+           $('.main').html(data);
+       })
+   }*/
+
+})
+;
+
+
+// shift panel on the first screen
 
 $('#shift').mousedown(function () {
     $(document).mousemove(function (e) {
-        if(e.pageX >= 0) {
+        if (e.pageX >= 0) {
             $('.scene-1-shifting-panel').css('left', e.pageX);
-        }else{
+        } else {
             return;
         }
         if (e.pageX <= $('.footer-2plus-block').offset().left) {
@@ -68,26 +150,3 @@ let virus = $('.virus');
 
 
 
-$(document).click(function () {
-    $('.scene-img-block div:nth-child(n+2)').fadeOut();
-    $('.scene-2').fadeOut(300, function () {
-
-        $.get('snippets/scene3-snippet.html', function (data) {
-            $('.scene-text').html(data).fadeIn(500);
-        });
-        $(this).addClass('scene-3');
-
-        $(".scene-img-block div:first-child").css('background-image', 'url(img/screen3-photo.png)').fadeIn(function () {
-
-            $(this).animate({
-                width: '390px',
-                height: '390px',
-            }, 500)
-
-
-        });
-    }).fadeIn(500, function () {
-        $('.ball').fadeIn();
-    });
-
-});
